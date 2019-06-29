8021ffbdf241630883d70bf1d7734750de8529d3
lexj <lexiongjin@outlook.com>
2019-06-29 17:08:37 +0800

    index finish

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="wrapper">
-  <swiper :options="swiperOption">
+  <swiper :options="swiperOption" v-if="showSwiper">
     <!-- slides -->
-    <swiper-slide v-for="item of swiperList" :key="item.id">
+    <swiper-slide v-for="item of list" :key="item.id">
       <div>
-       <img class="swiper-img" :src="item.imgurl" />
+       <img class="swiper-img" :src="item.imgUrl" />
       </div>
     </swiper-slide>
     <!-- Optional controls -->
@@ -16,19 +16,20 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgurl: 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
-      }, {
-        id: '0002',
-        imgurl: 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
-      }]
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
 }
--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div class='recommend-title'>热销推荐</div>
+    <div class='recommend-title'>周末去哪儿</div>
     <ul>
-      <li class='item border-bottom' v-for='item of recommendList' :key='item.id'>
+      <li class='item border-bottom' v-for='item of list' :key='item.id'>
         <div class='item-img-wrapper'>
           <img :src='item.imgUrl' class='item-img' />
         </div>
@@ -18,39 +18,8 @@
 <script>
 export default {
   name: 'HomeWeekend',
-  data () {
-    return {
-      recommendList: [
-        {
-          id: '0001',
-          imgUrl:
-            'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
-          title: '北京温泉排行榜',
-          desc: '细数北京温泉，温暖你的冬天'
-        },
-        {
-          id: '0001',
-          imgUrl:
-            'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
-          title: '北京温泉排行榜',
-          desc: '细数北京温泉，温暖你的冬天'
-        },
-        {
-          id: '0001',
-          imgUrl:
-            'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
-          title: '北京温泉排行榜',
-          desc: '细数北京温泉，温暖你的冬天'
-        },
-        {
-          id: '0001',
-          imgUrl:
-            'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
-          title: '北京温泉排行榜',
-          desc: '细数北京温泉，温暖你的冬天'
-        }
-      ]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
@@ -59,7 +28,6 @@ export default {
 @import '~styles/mixins.styl'
 
 .recommend-title {
-  margin-top: 0.2rem
   line-height: 0.8rem
   background: #ccc
   text-indent: 0.2rem
@@ -68,7 +36,7 @@ export default {
 .item-img-wrapper {
   height: 0
   overflow: hidden
-  padding-bottom: 33.9%
+  padding-bottom: 37.09%
 
   .item-img {
     width: 100%
